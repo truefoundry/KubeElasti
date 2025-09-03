424433745a226c521f228d337e8388f89cebeff8
Raman Tehlan <ramantehlan@gmail.com>
2025-09-04 03:10:30 +0530

    fix: fix tests

--- tests/e2e/tests/07-scale-down-with-filter/set_metric.sh
+++ tests/e2e/tests/07-scale-down-with-filter/set_metric.sh
@@ -2,4 +2,5 @@
 
 COMMAND="echo '${1} ${2}' | curl --data-binary @- http://prometheus-pushgateway.monitoring.svc.cluster.local:9091/metrics/job/some_job"
 
-kubectl exec -n default curl-target-gw -- sh -c "${COMMAND}"
+# Namespace is setup in 00-setup.yaml
+kubectl exec -n 7 curl-target-gw -- sh -c "${COMMAND}"
